9a37c80cc9cf138daf0b2dba8b96b0652d0459fa
rulex-bmw <github@rulex.tech>
2018-11-21 14:32:34 +0800

    start

---
+++ src/main/java/eroc/io/randx/RandxApplication.java
@@ -0,0 +1,12 @@
+package eroc.io.randx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandxApplication.class, args);
+	}
+}
---
+++ src/test/java/eroc/io/randx/RandxApplicationTests.java
@@ -0,0 +1,16 @@
+package eroc.io.randx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RandxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
